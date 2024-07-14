ab204af59be070a923ae53ffac569cfa3f18b83d
fiardiel <rafi.ardiel@ui.ac.id>
2024-07-14 23:31:55 +0700

    CHORE: Deployment stuff

--- flextime/settings.py
+++ flextime/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-l0^2ldqcvv8(tm4akn9p$u+hx_e--z(c#py5ywo_sy6q*fce&%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*", ".vercel.app", "127.0.0.1"]
 
 
 # Application definition
